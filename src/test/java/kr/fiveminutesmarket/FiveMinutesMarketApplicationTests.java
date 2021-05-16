d6f453d3b4f81e23fa9216c47d986c9f0e6bde82
beaniejoy <lhb1025@gmail.com>
2021-05-16 22:09:44 +0900

    프로젝트 셋팅 - issue #2
    - 프로젝트 초기 의존성 설정
    - README 수정: Code Convention 추가

---
+++ src/test/java/kr/fiveminutesmarket/FiveMinutesMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.fiveminutesmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveMinutesMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
