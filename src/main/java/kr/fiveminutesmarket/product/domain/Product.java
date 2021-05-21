6a0c770ec8bfc14b54ecfe7b525e809db56dc211
0n1dev <all_mind@naver.com>
2021-05-21 22:40:14 +0900

    상품관련 도메인 개발 - issue #5
    - 설정 파일 수정 (dev, prod 분리)
    - Product Entity 추가

---
+++ src/main/java/kr/fiveminutesmarket/product/domain/Product.java
@@ -0,0 +1,75 @@
+package kr.fiveminutesmarket.product.domain;
+
+public class Product {
+
+    private Long productId;
+
+    private Long mainCategoryId;
+
+    private Long subCategoryId;
+
+    private String name;
+
+    private Integer price;
+
+    private String thumb;
+
+    private String detail;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getMainCategoryId() {
+        return mainCategoryId;
+    }
+
+    public void setMainCategoryId(Long mainCategoryId) {
+        this.mainCategoryId = mainCategoryId;
+    }
+
+    public Long getSubCategoryId() {
+        return subCategoryId;
+    }
+
+    public void setSubCategoryId(Long subCategoryId) {
+        this.subCategoryId = subCategoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+}
