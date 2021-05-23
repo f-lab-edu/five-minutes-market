5c94c787dc1c7c1e1bf681663215f8b0c664cdca
beaniejoy <lhb1025@gmail.com>
2021-05-23 22:47:32 +0900

    상품관련 도메인 개발 - issue #5
    - 도메인 테이블 CREATE Query

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,3 +10,34 @@ CREATE TABLE `product` (
                            KEY `main_category_index` (`main_category_id`),
                            KEY `sub_category_index` (`sub_category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='상품 테이블'
+
+CREATE TABLE main_category
+(
+    main_category_id 		int AUTO_INCREMENT PRIMARY KEY,
+    main_category_name      VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE sub_category
+(
+    sub_category_id  		int AUTO_INCREMENT PRIMARY KEY,
+    sub_category_name       VARCHAR(50) NOT NULL,
+    main_category_id		BIGINT NOT NULL,
+    FOREIGN KEY (main_category_id) REFERENCES main_category (main_category_id)
+);
+
+CREATE TABLE `option`
+(
+    option_id 		        int AUTO_INCREMENT PRIMARY KEY,
+    is_optional		        boolean NOT NULL,
+    product_id		        int	NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (product_id)
+);
+
+CREATE TABLE option_item
+(
+    option_item_id 		    int AUTO_INCREMENT PRIMARY KEY,
+    option_item_name		VARCHAR(50) NOT NULL,
+    option_item_price		INT	NOT NULL,
+    option_id			    int NOT NULL,
+    FOREIGN KEY (option_id) REFERENCES `option` (option_id)
+);
