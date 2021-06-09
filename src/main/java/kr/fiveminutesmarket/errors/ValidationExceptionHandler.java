30b9e8aa9d1d9613e8141935736e7c32c4b37af5
beaniejoy <lhb1025@gmail.com>
2021-06-10 01:19:52 +0900

    review : Validation Exception Handler ResponseDto 적용

--- src/main/java/kr/fiveminutesmarket/errors/ValidationExceptionHandler.java
+++ src/main/java/kr/fiveminutesmarket/errors/ValidationExceptionHandler.java
@@ -1,49 +1,30 @@
 package kr.fiveminutesmarket.errors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
+import kr.fiveminutesmarket.common.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.ObjectError;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+@RestControllerAdvice
+public class ValidationExceptionHandler {
 
-@ControllerAdvice
-public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDto<?> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
 
-    public static final Logger logger = LoggerFactory.getLogger(ValidationExceptionHandler.class);
+        FieldError fieldError = bindingResult.getFieldError();
+        String message = "";
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
-        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        if(fieldError != null)
+            message = fieldError.getDefaultMessage();
 
-        List<String> errorMessages = allErrors.stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-
-        // TODO: errorResponse 설정 필요
-        return new ResponseEntity<>(errorMessages.toString(), status);
+        return new ResponseDto<>(-1, message);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
-
-        // TODO: errorResponse 설정 필요
-        return new ResponseEntity<>(ex.getMessage(), status);
-    }
 }
