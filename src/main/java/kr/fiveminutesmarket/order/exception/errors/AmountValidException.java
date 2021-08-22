658cab28b9cfa36b8abf0ea95c462ca034b49195
0n1dev <all_mind@naver.com>
2021-08-22 20:43:22 +0900

    refactor : 명시적인 예외 리팩토링
    - 명시적인 예외 리팩토링

---
+++ src/main/java/kr/fiveminutesmarket/order/exception/errors/AmountValidException.java
@@ -0,0 +1,8 @@
+package kr.fiveminutesmarket.order.exception.errors;
+
+public class AmountValidException extends RuntimeException {
+
+    public AmountValidException(String message) {
+        super(message);
+    }
+}
